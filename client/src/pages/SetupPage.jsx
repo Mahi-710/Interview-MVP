c26d67a3d28eef3d977fe1fb7af7fbfcbea2cb1c
sivaprasad447 <sivaprasadg4510@gmail.com>
2026-03-30 17:33:17 +0530

    update SetupPage

--- client/src/pages/SetupPage.jsx
+++ client/src/pages/SetupPage.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { useInterview } from '../context/InterviewContext';
